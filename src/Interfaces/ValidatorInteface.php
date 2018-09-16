963232bb1f6b0baabc4455d7ca3211e084c9ffe7
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-09-16 21:15:14 +0100

    feat: create the validator interface

---
+++ src/Interfaces/ValidatorInteface.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types = 1);
+
+namespace Forensic\Handler\Interfaces;
+
+interface ValidatorInterface
+{
+    public function setErrorBag(array &$error_bag);
+
+    public function getErrorBag(): array;
+}
