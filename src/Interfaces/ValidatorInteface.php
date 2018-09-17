ed341705dd2b2f5066c603c1210605dcf700c46b
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-09-17 08:05:11 +0100

    feat: extend the validator interface

--- src/Interfaces/ValidatorInteface.php
+++ src/Interfaces/ValidatorInteface.php
@@ -12,4 +12,28 @@ interface ValidatorInterface
     public function succeeds(): bool;
 
     public function fails(): bool;
+
+    /** text validation interface */
+    public function validateText(bool $required, string $field, $value, array $options): bool;
+
+    /** date validation interface */
+    public function validateDate(bool $required, string $field, $value, array $options): bool;
+
+    /** integer validation interface */
+    public function validateInteger(bool $required, string $field, $value, array $options): bool;
+
+    /** positive integer validation interface */
+    public function validatePInteger(bool $required, string $field, $value, array $options): bool;
+
+    /** negative integer validation interface */
+    public function validateNInteger(bool $required, string $field, $value, array $options): bool;
+
+    /** float validation interface */
+    public function validateFloat(bool $required, string $field, $value, array $options): bool;
+
+    /** positive float validation interface */
+    public function validatePFloat(bool $required, string $field, $value, array $options): bool;
+
+    /** negative float validation interface */
+    public function validateNFloat(bool $required, string $field, $value, array $options): bool;
 }
