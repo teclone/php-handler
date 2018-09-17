a6db338aaf164c6c125772b778cf921ff2533063
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-09-17 08:26:41 +0100

    test: test exception handling

--- tests/HandlerTest.php
+++ tests/HandlerTest.php
@@ -11,6 +11,67 @@ use Forensic\Handler\Exceptions\RuleNotFoundException;
 
 class HandlerTest extends TestCase
 {
+    /**
+     * returns test data
+     *
+     *@return array
+    */
+    public function getSimpleData()
+    {
+        return [
+            'first-name' => 'Harrison',
+            'last-name' => 'Ifeanyichukwu',
+            'age' => 22,
+            'password1' => 'random',
+            'password2' => '',
+            'timestamp' => '',
+            'date-of-birth' => '',
+        ];
+    }
+
+    /**
+     * returns test rules
+     *
+     *@return array
+    */
+    public function getSimpleRules()
+    {
+        return [
+            'first-name' => [
+                'type' => 'text',
+                'hint' => 'first-name field is required',
+                'options' => [
+                    'min' => 3,
+                    'max' => 15,
+                ],
+            ],
+            'last-name' => [
+                'type' => 'text'
+            ],
+            'middle-name' => [
+                'required' => false
+            ],
+            'age' => [
+                'type' => 'int',
+                'required' => false
+            ],
+            'password1' => [
+                'type' => 'text'
+            ],
+            'password2' => [
+                'required' => false,
+                'default' => '{password1}'
+            ],
+            'timestamp' => [
+                'required' => false,
+                'default' => '{CURRENT_TIME}'
+            ],
+            'date-of-birth' => [
+                'required' => false,
+                'default' => '{CURRENT_DATE}'
+            ],
+        ];
+    }
 
     /**
      * test that we can create an instance without any argument
@@ -52,5 +113,34 @@ class HandlerTest extends TestCase
 
         $this->expectException(DataSourceNotRecognizedException::class);
         $instance = new Handler('put');
+    }
+
+    /**
+     * test create instance with rule
+    */
+    public function testCreateInstanceWithRules()
+    {
+        $instance = new Handler(null, $this->getSimpleRules());
+        $this->assertInstanceOf(Handler::class, $instance);
+    }
+
+    /**
+     * test that it throws error if executed with no data set
+    */
+    public function testExecuteWithNoDataSet()
+    {
+        $this->expectException(DataNotFoundException::class);
+        $instance = new Handler();
+        $instance->execute();
+    }
+
+    /**
+     * test that it throws error if executed with no rule set
+    */
+    public function testExecuteWithNoRulesSet()
+    {
+        $this->expectException(RuleNotFoundException::class);
+        $instance = new Handler($this->getSimpleData());
+        $instance->execute();
     }
 }
