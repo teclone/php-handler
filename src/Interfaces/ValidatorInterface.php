82d4cb0b2eae9a245922ae40753bb4d1a7535e3c
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-10-03 07:06:24 +0100

    refactor: add the validateFile public interface, also require current validation value index
    the index helps in handling mutifile field validation

--- src/Interfaces/ValidatorInterface.php
+++ src/Interfaces/ValidatorInterface.php
@@ -14,38 +14,54 @@ interface ValidatorInterface
     public function fails(): bool;
 
     /** text validation interface */
-    public function validateText(bool $required, string $field, $value, array $options): bool;
+    public function validateText(bool $required, string $field, $value,
+        array $options, int $index = 0): bool;
 
     /** date validation interface */
-    public function validateDate(bool $required, string $field, $value, array $options): bool;
+    public function validateDate(bool $required, string $field, $value,
+        array $options, int $index = 0): bool;
 
     /** integer validation interface */
-    public function validateInteger(bool $required, string $field, $value, array $options): bool;
+    public function validateInteger(bool $required, string $field, $value,
+        array $options, int $index = 0): bool;
 
     /** positive integer validation interface */
-    public function validatePInteger(bool $required, string $field, $value, array $options): bool;
+    public function validatePInteger(bool $required, string $field, $value,
+        array $options, int $index = 0): bool;
 
     /** negative integer validation interface */
-    public function validateNInteger(bool $required, string $field, $value, array $options): bool;
+    public function validateNInteger(bool $required, string $field, $value,
+        array $options, int $index = 0): bool;
 
     /** float validation interface */
-    public function validateFloat(bool $required, string $field, $value, array $options): bool;
+    public function validateFloat(bool $required, string $field, $value,
+        array $options, int $index = 0): bool;
 
     /** positive float validation interface */
-    public function validatePFloat(bool $required, string $field, $value, array $options): bool;
+    public function validatePFloat(bool $required, string $field, $value,
+        array $options, int $index = 0): bool;
 
     /** negative float validation interface */
-    public function validateNFloat(bool $required, string $field, $value, array $options): bool;
+    public function validateNFloat(bool $required, string $field, $value,
+        array $options, int $index = 0): bool;
 
     /** email validation interface */
-    public function validateEmail(bool $required, string $field, $value, array $options): bool;
+    public function validateEmail(bool $required, string $field, $value,
+        array $options, int $index = 0): bool;
 
     /** url validation interface */
-    public function validateURL(bool $required, string $field, $value, array $options): bool;
+    public function validateURL(bool $required, string $field, $value,
+        array $options, int $index = 0): bool;
 
     /** choice validation interface */
-    public function validateChoice(bool $required, string $field, $value, array $options): bool;
+    public function validateChoice(bool $required, string $field, $value,
+        array $options, int $index = 0): bool;
 
     /** range validation interface */
-    public function validateRange(bool $required, string $field, $value, array $options): bool;
+    public function validateRange(bool $required, string $field, $value,
+        array $options, int $index = 0): bool;
+
+    /** file validation interface */
+    public function validateFile(bool $required, string $field, $value,
+        array $options, int $index = 0, string &$new_value = null): bool;
 }
