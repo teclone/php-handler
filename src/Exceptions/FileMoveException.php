41ff25250df7ed9dc3f6f9a7046873cd8cb5b64d
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-10-05 13:04:27 +0100

    feat: create generic FileMoveException class

---
+++ src/Exceptions/FileMoveException.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types = 1);
+
+namespace Forensic\Handler\Exceptions;
+use Exception;
+
+class FileMoveException extends Exception
+{
+    public function __construct(string $message, int $code = 0, Exception $previous = null)
+    {
+        Exception::__construct($message, $code, $previous);
+    }
+}
