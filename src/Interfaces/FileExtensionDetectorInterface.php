0807c2a5fb2a2806d2c7b5c1cf4d93a330f12853
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-10-05 13:39:27 +0100

    feat: expose the resolveExtension(s) method interfaces

--- src/Interfaces/FileExtensionDetectorInterface.php
+++ src/Interfaces/FileExtensionDetectorInterface.php
@@ -5,6 +5,12 @@ namespace Forensic\Handler\Interfaces;
 
 interface FileExtensionDetectorInterface
 {
+    /** resolves a given extension to something compatible with its internal extension */
+    public function resolveExtension(string $ext): string;
+
+    /** resolves array of extensions to something compatible with its internal extensions */
+    public function resolveExtensions(array $exts): array;
+
     /** detect file extension */
     public function detect(string $filename): array;
 }
