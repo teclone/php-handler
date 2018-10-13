eddf77bbd7d2a63f1bfe2f17ceac78f81eae2905
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-10-13 07:38:08 +0100

    feat: add interfaces that returns  mimes for image, video, audio, media and document files

--- src/Interfaces/FileExtensionDetectorInterface.php
+++ src/Interfaces/FileExtensionDetectorInterface.php
@@ -5,6 +5,21 @@ namespace Forensic\Handler\Interfaces;
 
 interface FileExtensionDetectorInterface
 {
+    /** returns array of image file mimes */
+    public function getImageMimes(): array;
+
+    /** returns array of audio file mimes */
+    public function getAudioMimes(): array;
+
+    /** returns array of video file mimes */
+    public function getVideoMimes(): array;
+
+    /** returns array of media file mimes */
+    public function getMediaMimes(): array;
+
+    /** returns array of document file mimes */
+    public function getDocumentMimes(): array;
+
     /** resolves a given extension to something compatible with its internal extension */
     public function resolveExtension(string $ext): string;
 
