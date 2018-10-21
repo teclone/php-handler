fb375161161f037eae99da42f32d93b6c0e1281e
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-10-21 19:52:32 +0100

    refactor: make array operation methods to accept array of string keys or a single string key

--- src/Util.php
+++ src/Util.php
@@ -9,40 +9,6 @@ namespace Forensic\Handler;
 class Util
 {
     /**
-     * returns the value for the given array key if it exists, otherwise, return the default
-     * value
-     *
-     *@param string $key - the array key
-     *@param array $arr - the array
-     *@param mixed [$default=null] - the default value to return if otherwise
-     *@return mixed
-    */
-    public static function value(string $key, array $arr, $default = null)
-    {
-        if (array_key_exists($key, $arr))
-            return $arr[$key];
-        else
-            return $default;
-    }
-
-    /**
-     * returns array value for the given array key if key exists and its value is an array
-     * otherwise, return the default value
-     *
-     *@param string $key - the array key
-     *@param array $arr - the array
-     *@param array [$default=[]] - the default value to return if otherwise
-     *@return array
-    */
-    public static function arrayValue(string $key, array $arr, array $default = [])
-    {
-        if (array_key_exists($key, $arr) && is_array($arr[$key]))
-            return $arr[$key];
-        else
-            return $default;
-    }
-
-    /**
      * returns true if the given key is not set or if the key is set and it is truthy
      *
      *@param string $key - the array key
@@ -87,18 +53,6 @@ class Util
     }
 
     /**
-     * unsets an entry from an array if the key exists
-     *
-     *@param string $key - the array key
-     *@param array $arr - the array
-    */
-    public static function unsetFromArray(string $key, array &$arr)
-    {
-        if (array_key_exists($key, $arr))
-            unset($arr[$key]);
-    }
-
-    /**
      * puts the value inside an array and returns the resulting array or returns the value if
      * it is already an array
      *
@@ -111,5 +65,64 @@ class Util
             return $value;
         else
             return array($value);
+    }
+
+    /**
+     * returns the value for the first key in the keys array that exists in the array
+     * otherwise, return the default value
+     *
+     *@param string[]|string $keys - array of keys or a single string key
+     *@param array $arr - the array
+     *@param mixed [$default=null] - the default value to return if otherwise
+     *@return mixed
+    */
+    public static function value($keys, array $arr, $default = null)
+    {
+        $keys = self::makeArray($keys);
+        foreach($keys as $key)
+        {
+            if (array_key_exists($key, $arr))
+                return $arr[$key];
+        }
+
+        return $default;
+    }
+
+    /**
+     * returns the value for the first key in the keys array whose value is an array that
+     * exists in the array
+     * otherwise, return the default value
+     *
+     *@param string[]|string $keys - array of keys or a single string key
+     *@param array $arr - the array
+     *@param mixed [$default=[]] - the default value to return if otherwise
+     *@return array
+    */
+    public static function arrayValue($keys, array $arr, array $default = [])
+    {
+        $keys = self::makeArray($keys);
+        foreach($keys as $key)
+        {
+            if (array_key_exists($key, $arr) && is_array($arr[$key]))
+                return $arr[$key];
+        }
+
+        return $default;
+    }
+
+    /**
+     * unsets array of keys from the given array if it exists in the array
+     *
+     *@param string[]|string $keys - array of keys or a single string key
+     *@param array $arr - the array
+    */
+    public static function unsetFromArray($keys, array &$arr)
+    {
+        $keys = self::makeArray($keys);
+        foreach($keys as $key)
+        {
+            if (array_key_exists($key, $arr))
+                unset($arr[$key]);
+        }
     }
 }
